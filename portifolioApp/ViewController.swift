4dc5ca4413f2ef13e4641a626ba25cb42694efcc
Guilherme <schmitgui@gmail.com>
2023-02-09 15:07:23 +0100

    Initial Commit

---
+++ portifolioApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  portifolioApp
+//
+//  Created by Guilherme Schmit Dall Agnol on 09.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
